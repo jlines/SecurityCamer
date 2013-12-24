31c3a6adf3c4132e467be32838ed3988d8cf7a6d
Jason Lines <jlines@solidstatenetworks.com>
2013-12-23 17:57:49 -0700

    1

--- routes/camera.js
+++ routes/camera.js
@@ -1,3 +1,53 @@
 /**
  * Created by jasonlines on 12/23/13.
  */
+var RaspiCam = require("raspicam"),
+    nodemailer = require("nodemailer");
+
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "jlines2009@gmail.com",
+        pass: "p1at3au!!"
+    }
+});
+
+var camera = new RaspiCam({
+    mode:"photo",
+    output:"./pictures/" + new Date().getTime() + ".jpg"
+});
+
+console.log("camera init");
+
+exports.sendImage = function(req, res){
+    res.send("emailing photo");
+
+    //to take a snapshot, start a timelapse or video recording
+    camera.start( );
+};
+
+//listen for the "read" event triggered when each new photo/video is saved
+camera.on("read", function(err, filename){
+
+    var mailOptions = {
+        from: "jlines <jlines@blurdybloop.com>", // sender address
+        to: "commanderjason@gmail.com", // list of receivers
+        subject: "Image captured at " + new Date().toISOString(), // Subject line
+        text: "New Image",
+        attachments: {
+            filePath:filename
+        }
+    }
+
+// send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log("Message sent: " + response.message);
+        }
+
+        // if you don't want to use this transport object anymore, uncomment following line
+        //smtpTransport.close(); // shut down the connection pool, no more messages
+    });
+});
